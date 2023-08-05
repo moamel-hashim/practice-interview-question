255419e42c95afd3c8b63884a9f13cbdc0cb67df
moamel-hashim <muamal1997@gmail.com>
2023-08-04 18:09:09 -0700

    solved suarray sum

--- interview/main.js
+++ interview/main.js
@@ -481,3 +481,20 @@ return that
 // };
 
 // console.log(numOfSubarrays([1, 2, 2, 3, 4, 5, 10], 3, 4));
+
+// var minSubArrayLen = function(target, nums) {
+//     let result = Number.MAX_SAFE_INTEGER;
+//     let sum = 0;
+//     let start = 0;
+//     for(let i = 0; i < nums.length; i++) {
+//         sum += nums[i]
+//         while(sum >= target) {
+//             result = Math.min(result, i - start + 1);
+//             sum -= nums[start]
+//             start++;
+//         }
+//     } if(result === Number.MAX_SAFE_INTEGER) {
+//         return 0;
+//     }
+//     return result;
+// };
